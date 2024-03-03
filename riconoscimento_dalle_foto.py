73d4874149088348eb56c514e45604f9943e6673
arduinodenis <90153974+ArduinoDenis@users.noreply.github.com>
2024-03-03 19:42:08 +0100

    Update riconoscimento_dalle_foto.py

--- riconoscimento_dalle_foto.py
+++ riconoscimento_dalle_foto.py
@@ -1,14 +1,25 @@
-import face_recognition as riconoscimento
+import face_recognition as fr
 from glob import glob
 from shutil import move
 
-immagine_da_riconoscere = riconoscimento.load_image_file("") #nelle virgolette mettere la foto da far riconoscere
-encoding_da_riconoscere = riconoscimento.face_encodings(immagine_da_riconoscere)[0]
+# Load the image to be recognized
+image_to_recognize = fr.load_image_file("path/to/image.jpg")  # Insert the path to the image to be recognized within the quotes
 
-for foto in glob(""): #percorso della cartella es fotodariconoscere/*.png
-    print(foto)
-    immagine = riconoscimento.load_image_file(foto)
-    encoding = riconoscimento.face_encodings(immagine)[0]
-    match = riconoscimento.compare_faces([encoding_da_riconoscere], encoding)[0]
+# Encode the image to be recognized
+encoding_to_recognize = fr.face_encodings(image_to_recognize)[0]
+
+# Loop through all images in the specified folder
+for photo in glob("folder_path/*.png"):  # Replace "folder_path" with the path to the folder containing the images to be recognized
+    print(photo)
+    # Load the current image
+    image = fr.load_image_file(photo)
+    
+    # Encode the current image
+    encoding = fr.face_encodings(image)[0]
+    
+    # Compare the encodings of the current image and the image to be recognized
+    match = fr.compare_faces([encoding_to_recognize], encoding)[0]
+    
+    # If a match is found, move the image to a specified folder
     if match:
-        move(foto, "") #creare una cartella e "" mettere il percorso della cartella
+        move(photo, "destination_folder_path/")  # Replace "destination_folder_path" with the path to the destination folder
